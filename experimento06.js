d70c82228251fb11346e200a3b19bc985a01d1c0
Debora <debora.cyriaco@escola.pr.gov.br>
2026-08-27 10:33:16 +0000

    função js06

--- experimento06.js
+++ experimento06.js
@@ -1,6 +1,13 @@
 const botaoAbrir = document.getElementById("btnAbrirModal");
 const modalContainer = document.getElementById("modalContainer");
 
-botaoAbrir.addEventListener("click"; function(){
-    
+botaoAbrir.addEventListener("click", function(){
+    modalContainer.innerHTML = `
+            <div class="modal-fundo">
+               <div class="modal-caixa">
+                  <h2>Aviso!</h2>
+                  <p>Este modal inteiro foi criado pelo JavaScript.</p>
+                   <button id="btnFechaModal">Fecha</button>
+               </div>
+           </div>`
 })
